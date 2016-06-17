f75a10c3a686fd99751978bb44647821202b90ac
bhuber2010 <bhuber@alumni.colostate.edu>
2016-06-17 15:17:35 -0700

    removed express-handlebars

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var exphbs  = require('express-handlebars');
 
 var routes = require('./routes/index');
 var formres = require('./routes/formres');
